6af592d7a0cb675f4cc2d4109ee304755f08cb28
Joshua Randle <randlejo@oregonstate.edu>
2023-03-05 17:28:41 -0600

    Test 2 checks to see if password more than 20 characters returns False.

--- tests.py
+++ tests.py
@@ -6,6 +6,10 @@ class TestCase(unittest.TestCase):
     # Test 1 checks to see if password less than 8 characters returns False
     def test1(self):
         self.assertFalse(check_pwd("1234567"))
+    
+    # Test 2 checks to see if password more than 20 characters returns False
+    def test2(self):
+        self.assertFalse(check_pwd("123456789123456789123"))
 
 if __name__ == '__main__':
     unittest.main()
