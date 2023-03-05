e4c11463cdac7ffe38abefd1d4a075b8a9b94e7e
Joshua Randle <randlejo@oregonstate.edu>
2023-03-05 17:24:56 -0600

    Test 1 checks to see if password less than 8 characters returns False.

--- tests.py
+++ tests.py
@@ -3,8 +3,9 @@ from check_pwd import check_pwd
 
 
 class TestCase(unittest.TestCase):
-    pass
-
+    # Test 1 checks to see if password less than 8 characters returns False
+    def test1(self):
+        self.assertFalse(check_pwd("1234567"))
 
 if __name__ == '__main__':
     unittest.main()
