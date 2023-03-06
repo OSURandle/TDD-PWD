29ba5f5cedbaddca4dbab821f5d2be2e90746468
Joshua Randle <randlejo@oregonstate.edu>
2023-03-06 02:14:45 -0600

    Made check_pwd return False when password matched all specifications but with no digits. Passed.

--- check_pwd.py
+++ check_pwd.py
@@ -2,7 +2,10 @@ def check_pwd(pwd):
     symbols = "~`!@#$%^&()_+-="
     if len(pwd) < 8 or len(pwd) > 20:
         return False
-
+# Citation for the following function:
+# Date: 03/05/2023
+# Copied from /OR/ Adapted from /OR/ Based on:
+# https://canvas.oregonstate.edu/courses/1901830/pages/exploration-random-testing?module_item_id=22851350
     if not any(char.isdigit() for char in pwd):
         return False
 
@@ -11,6 +14,11 @@ def check_pwd(pwd):
 
     if pwd.isupper():
         return False
+
+# Citation for the following function:
+# Date: 03/05/2023
+# Copied from /OR/ Adapted from /OR/ Based on:
+# https://canvas.oregonstate.edu/courses/1901830/pages/exploration-random-testing?module_item_id=22851350
 
     if not any(char in symbols for char in pwd):
         return False
